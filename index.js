d4687f1ae8b890f650d11c127d061e71e2f26432
Kairos22 <alandiko84@gmail.com>
2022-08-22 17:39:54 +0200

    recursion

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1
+function sum(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+
+    return arr.pop() + sum(arr);
+}
+
+const array = [1, 2, 3, 4, 5, 6]
+    
+console.log(sum(array)); 
